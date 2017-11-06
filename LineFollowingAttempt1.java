1ff044c502f3562049ee22268b3c370fc598e8a4
K1630593 <K1630593@nmscde003899.nms.kcl.ac.uk>
2017-11-06 10:49:53 +0000

    Some edits on the LineFollowingAttempt1 code.

--- LineFollowingAttempt1.java
+++ LineFollowingAttempt1.java
@@ -2,14 +2,17 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
+import lejos.hardware.Key;
 
 /*
  * This will help me to determint which values are returned for black, and which are returned for white. This should allow me to write a program that
@@ -19,30 +22,50 @@ import lejos.hardware.BrickFinder;
 public class LineFollowingAttempt1 {
 
 	public LineFollowingAttempt1() {
-		//initialise colorSensor and set it to the correct port
-		Brick brick = new BrickFinder.getDefault();
-		Key escapeKey = brick.getKey("escpae");
+		//motorA is the small motor at the top which we will not use for now
+		EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
+		
+		//declaring the Brick and brickfinder may not be necessary as the brick has already been connected in the preferences
+		//up to you tbh
+		new BrickFinder();
+		Brick brick = BrickFinder.getDefault();
+		
+		/*
+		Key escapeKey = brick.getKey("escape"); //this is not necessary
 		Port s1 = LocalEV3.get().getPort("S1");
 
 		//configure the colorSensor and it's redMode
     	EV3ColorSensor colorSensor = new EV3ColorSensor(s1);
+    	*/
+		
+        Port s2 = brick.getPort("S2");
+        EV3ColorSensor sensor = new EV3ColorSensor(s2);
+        
+        while(sensor.getColorID() == 1){ //colour ID 0-7, 1 is black
+        	//going to need to use multithreading to run these two methods at the same time 
+        	motorB.rotate(60); 
+        	motorC.rotate(-60);
+        } 
+        
+        /*
 		SampleProvider redMode = colorSensor.getRedMode();
 		float[] colorSample = new float[redMode.sampleSize()];
 
 		//method to test the output of using the color sensor
-		while(escape.isDown() == false) {
+		while(escapeKey.isDown() == false) {
 
 			redMode.fetchSample(colorSample, 0);
 			System.out.println(redMode.fetchSample(colorSample, 0));
 
 		}
+		*/
 
-		public static void main(String[] args) {
+	}
+	public static void main(String[] args) {
 
-			LineFollowingAttempt1 testThing = new LineFollowingAttempt1();
+		LineFollowingAttempt1 testThing = new LineFollowingAttempt1();
 
-
-		}
 
 	}
 	
