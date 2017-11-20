fcb6fa326833577e7efa87b97d3e145c68b7fec5
k1630593 <k1630593@nmscde003899.nms.kcl.ac.uk>
2017-11-20 10:03:45 +0000

    partial logic -> code

--- UltrasonicSensorTest.java
+++ UltrasonicSensorTest.java
@@ -8,8 +8,10 @@ public class UltrasonicSensorTest {
     private EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(SensorPort.S3);
     private SampleProvider ultraMode = ultraSensor.getDistanceMode();
     private float[] ultrasonicSample = new float[ultraMode.sampleSize()];
-
+    
+    private boolean linePresent = false;
     private int sensorAngle = 0;
+    
     //if we reach an obstacle less than 20 cm away, begin turning right
     float lastReading = ultraMode.fetchSample(ultrasonicSample, 0);
 
@@ -23,23 +25,24 @@ public class UltrasonicSensorTest {
         lastReading = ultraMode.fetchSample(ultrasonicSample, 0);
     }
     
-    double increasing = 1.0; //just a read for if the distance from the sensor is increasing or not
+    //we should now be half way past the obstacle
     
-    while(lR2 > 0;){ //or until a black line is found
-        leftMotor.rotate(60); //the sensor should still be rotating in the same direction
-        sensorAngle = sensorAngle + 60;
+    while(lastReading > 0.15 && lastReading <  0.2){ //logic for when the sensor first finds an obstacle
+        leftMotor.rotate(60);
+        sensorMotor.rotate(60);
+        sensorAngle = + sensorAngle + 60;
         rightMotor.rotate(60); //trying to move it forward
         leftMotor.rotate(60);
-        double lR2 = ultraMode.fetchSample(ultrasonicSample, 0);
-        increasing = lr2 - lastReading;
-        lastReading = lr2;
+        lastReading = ultraMode.fetchSample(ultrasonicSample, 0);
     }
+
     //^ we keep track of the angle of the sensor so we can
     // 1. correct it when the obstacle has passed
     // 2. have some way of keeping track how off course the robot is from the original track
 
     //turn the sensor back to facing the front using the sensor angle
-    
+    //when a black line is found:
+    sensorMotor.rotate(-sensorAngle);                                               
     
 	//just see what might be used for the ultrasonic range
 	/*
