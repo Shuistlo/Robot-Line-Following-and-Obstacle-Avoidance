a3001a44bfec25d6bc92bac2bee49e44c03cb636
Ziling Xu <ziling.xu@kcl.ac.uk>
2017-11-20 02:50:03 +0000

    notes for the ultrasonic sensor

--- UltrasonicSensorTest.java
+++ UltrasonicSensorTest.java
@@ -1,4 +1,6 @@
 public class UltrasonicSensorTest {
+	
+	
     private RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
     private RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D); //PORT C IS BROKE AS FUCK
     private RegulatedMotor sensorMotor = new EV3LargeRegulatedMotor(MotorPort.B);
@@ -10,6 +12,7 @@ public class UltrasonicSensorTest {
     private int sensorAngle = 0;
     //if we reach an obstacle less than 20 cm away, begin turning right
     float lastReading = ultraMode.fetchSample(ultrasonicSample, 0);
+
 
     while(lastReading > 0.15 && lastReading <  0.2){ //logic for when the sensor first finds an obstacle
         rightMotor.turn(60);
@@ -29,4 +32,19 @@ public class UltrasonicSensorTest {
     // 2. have some way of keeping track how off course the robot is from the original track
 
     //turn the sensor back to facing the front using the sensor angle
+    
+    
+	//just see what might be used for the ultrasonic range
+	/*
+	 * set up new differential pilot(not sure)
+	 * set up brick
+	 * Brick b = BrickFinder.getDefault();
+	 * set up sensor port
+	 * create new EV3UltrasonicSensor (already mentioned)
+	 * new Ultrasonic(getMode("Distance"))
+	 */
+    
+    //another way we probably can do, maybe we can create an ultrasonic distance class 
+    //should we stop the robot first once finds the obstacle, call the float data pulled in
+    //check the distance, if the distance is less than () cm, robot stops, else if...
 }
