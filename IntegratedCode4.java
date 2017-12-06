79e20b2d3943924837828b22bf7de53cc573134d
AndreiND <32303167+AndreiND@users.noreply.github.com>
2017-12-06 17:56:29 +0000

    finished obstacle avoidance

--- IntegratedCode4.java
+++ IntegratedCode4.java
@@ -7,24 +7,25 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
+　
 public class IntegratedCode4 {
 
 	//Initialising error, integral and derivative constants
 	//These will need to be adjusted appropriately
 	public static final double kp = 285;
-	public static final double ki = 1.5;
+	public static final double ki = 0.25;
 	public static final double kd = 15;
 
 	//Upon testing the sensor on the actual track, 0.45 was the value the observed value of having the sensor detecting exactly half of the line.
 	public static final double target = 0.45;
 	private double error = 0, integral = 0, last_error = 0, derivative;
 	private int steeringValue;
-	final double eq = 0.15;
+	final double eq = 0.075;
 	final double p = 200;
-	final double i = 0.5;
-	final double d = 5;
-	private double e = 0, in = 0, l_e = 0, der = 0, last_e = 0;
+	final double d = 25;
+	private double e = 0, der = 0, last_e = 0;
 	double obstTurnVal = 0;
 
 	//Adjust as necessary
@@ -32,7 +33,7 @@ public class IntegratedCode4 {
 	RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D); //PORT C IS BROKE AS FUCK
     private RegulatedMotor sensorMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 
-    private EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(SensorPort.S4);
+    private EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(SensorPort.S3);
     private SampleProvider ultraMode = ultraSensor.getDistanceMode();
     private float[] ultrasonicSample = new float[ultraMode.sampleSize()];
 
@@ -45,7 +46,7 @@ public class IntegratedCode4 {
 	public static void main(String[] args) {
 
 		IntegratedCode4 test = new IntegratedCode4();
-		test.move(150, 100);
+		test.move(150, 125);
 
 	}
 
@@ -68,12 +69,15 @@ public class IntegratedCode4 {
 		}*/
 		
 		while(true) {
-		
-			while(ultrasonicSample[0] > 0.2) {
 			
+			leftMotor.setSpeed(100); rightMotor.setSpeed(100);
+			leftMotor.forward(); rightMotor.forward();
+		
+			while(ultrasonicSample[0] > 0.075) {
+				
 				redMode.fetchSample(colorSample, 0);
 				ultraMode.fetchSample(ultrasonicSample, 0);
-				System.out.println(colorSample[0]);
+				//System.out.println(colorSample[0]);
 				//implement PID control in order to get the appropriate steering value
 				error = (double) target - colorSample[0];
 				integral += error;	
@@ -107,33 +111,45 @@ public class IntegratedCode4 {
 			}
 			
 			rightMotor.stop(); leftMotor.stop();
-			rightMotor.rotate(-105);
-			leftMotor.rotate(105);
-			sensorMotor.rotate(-75);
+			rightMotor.rotate(-130);
+			leftMotor.rotate(130);
+			sensorMotor.rotate(-85);
 			obstTurnVal = 0;
-			rightMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));
-			leftMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));
+			//rightMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));
+			//leftMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));
 			rightMotor.forward(); leftMotor.forward();
 
 			while(colorSample[0] > 0.2) {
 			
 				ultraMode.fetchSample(ultrasonicSample, 0);
 				redMode.fetchSample(colorSample, 0);
-				e = eq - colorSample[0];
-				in += e;
+				e = eq - ultrasonicSample[0];
 				der = e + last_e;
-				obstTurnVal = (int)((p * e) + (i * in) + (d * der));
+				obstTurnVal = (int)((p * e) + (d * der));
+				System.out.println(ultrasonicSample[0]);
 				
-				if (obstBaseSpeed - obstTurnVal > 300) {rightMotor.setSpeed(300);}
-				else {rightMotor.setSpeed((int)(obstBaseSpeed - obstTurnVal));}
-				if(obstBaseSpeed + obstTurnVal > 300) {leftMotor.setSpeed(300);}
-				else {leftMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));}
+				if(ultrasonicSample[0] < 0.2) {
+				
+					if (obstBaseSpeed - obstTurnVal > 300) {rightMotor.setSpeed(300);}
+					else {rightMotor.setSpeed((int)(obstBaseSpeed - obstTurnVal));}
+					if(obstBaseSpeed + obstTurnVal > 300) {leftMotor.setSpeed(300);}
+					else {leftMotor.setSpeed((int)(obstBaseSpeed + obstTurnVal));}
+					
+				} else {rightMotor.setSpeed(100); leftMotor.setSpeed(50);}
 				
 				last_e = e;
 				
 			}
-
-			sensorMotor.rotate(75);	
+			
+			rightMotor.stop(); leftMotor.stop();
+			sensorMotor.rotate(75);
+			Delay.msDelay(1000);
+			ultraMode.fetchSample(ultrasonicSample, 0);
+			rightMotor.rotate(-120);
+			leftMotor.rotate(150);
+			
+			
+			
 		}
 
 　
